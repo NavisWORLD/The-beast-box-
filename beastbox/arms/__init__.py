ead6fd796d50542b99972113a497edf19fd49b82
NavisWORLD <personal.phera@gmail.com>
2026-08-15 22:57:15 -0500

    feat: expose Beast Arms evidence primitives

---
+++ beastbox/arms/__init__.py
@@ -0,0 +1,12 @@
+"""Defensive Networked Cage benchmark primitives."""
+
+from beastbox.arms.recorder import EvidenceRecorder
+from beastbox.arms.schema import EventEnvelope, RunConfig, ToolRequest, ToolResult
+
+__all__ = [
+    "EvidenceRecorder",
+    "EventEnvelope",
+    "RunConfig",
+    "ToolRequest",
+    "ToolResult",
+]
