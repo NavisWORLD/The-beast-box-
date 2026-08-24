92fe2b0572f4b0e20c56e71b795fa34944be879e
Phera aka sunflower <personal.phera@gmail.com>
2026-08-23 21:19:18 -0500

    test: trigger Probe 001 red contract

--- tests/test_r12_physics_probe.py
+++ tests/test_r12_physics_probe.py
@@ -4,4 +4,5 @@ import importlib.util
 
 
 def test_probe001_core_module_exists() -> None:
+    """RED gate: production module must not exist until this test is observed failing."""
     assert importlib.util.find_spec("beastbox.r12_physics_probe") is not None
