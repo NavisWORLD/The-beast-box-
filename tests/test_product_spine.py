bb39e71b88faeee36c05a7e3e84bcd7e60788ef2
Phera aka sunflower <personal.phera@gmail.com>
2026-09-12 16:43:38 -0500

    test: expect Beast Box 0.7.1 package identity

--- tests/test_product_spine.py
+++ tests/test_product_spine.py
@@ -20,7 +20,7 @@ from beastbox.state import MissionState
 
 
 def test_version_and_aliases():
-    assert __version__ == "0.7.0"
+    assert __version__ == "0.7.1"
     assert "Zeref" in HISTORICAL_ALIASES
     assert Runtime is CosmosRuntime
     assert MemoryStore is ReconciliationMemory
